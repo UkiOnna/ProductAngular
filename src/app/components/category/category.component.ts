738624a196c39967cb481e2ae019479d15766ab2
UkiOnna <sikirovtemirlan@mail.ru>
2019-10-31 14:05:49 +0600

    do category for testing

--- src/app/components/category/category.component.ts
+++ src/app/components/category/category.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CategoriesService } from 'src/app/services/category.services';
+import { Category } from 'src/app/Entities/Category';
+
 
 @Component({
   selector: 'app-category',
@@ -6,10 +10,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./category.component.scss']
 })
 export class CategoryComponent implements OnInit {
+  public categories:Category[];
+  constructor(private categoryService: CategoriesService,
+    private route: ActivatedRoute) {
 
-  constructor() { }
+  }
 
   ngOnInit() {
+    this.categoryService.getCategories().subscribe(result => this.acceptCategories(result));
+  }
+
+  private acceptCategories(categories: Category[]) {
+    categories.forEach(r => {
+      let category: Category;
+      category.name = r.name;
+      this.categories.push(category);
+    })
+
   }
 
 }
